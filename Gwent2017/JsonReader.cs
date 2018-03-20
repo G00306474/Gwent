4d5c13c8f96cca9894d91a8f17128704a0a4c121
G00306474 <G00306474@gmit.ie>
2018-03-20 23:00:04 +0000

    Json File Added(test version)

---
+++ Gwent2017/JsonReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+
+namespace Gwent2017
+{
+    [DataContract]
+    public class GwentCards
+    {
+        private string _code = string.Empty;
+        [DataMember]
+        public string name
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string faction
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string positions
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public int strength
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string art
+        {
+            get;
+            set;
+        }
+    }
+}
--- Gwent2017/MainPage.xaml.cs
+++ Gwent2017/MainPage.xaml.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +26,30 @@ namespace Gwent2017
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
+
         public MainPage()
         {
-             this.InitializeComponent();
+            this.InitializeComponent();
         }
-    }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Name == "button") { Deal(); }
+            if (Name == "button1") {  }
+            if (Name == "button2") {  }
+        }
+        public void Deal()
+        {
+
+            string FilePath = Path.Combine(Package.Current.InstalledLocation.Path, "test.json");
+            using (StreamReader file = File.OpenText(FilePath))
+            {
+                var json = file.ReadToEnd();
+                Dictionary<string, object> result = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                string contacts = result["test"].ToString();
+                List<GwentCards> objResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<GwentCards>>(contacts);
+                gwentJsonCardTester.ItemsSource = objResponse;
+            }
+        } 
+    }//close mainpage
 }
